56de2bdde4e6e9c259afc48f3ec0d17953485aa8
mfeke <mfekethothulane@gmail.com>
2023-09-28 10:18:27 +0200

    update

---
+++ app/src/app/services/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UserService {
+
+  api = "http://localhost/3300/chat"
+
+  
+
+  constructor( private http:HttpClient) { }
+  
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(this.api+ 'signin', {
+      username,
+      password
+    }, httpOptions);
+  }
+
+
+}
