0949d57c395805eeb378302ee6ba5f2be4333186
mfeke <mfekethothulane@gmail.com>
2023-10-05 17:25:14 +0200

    update

--- app/src/app/components/chats-home/chats-home.component.ts
+++ app/src/app/components/chats-home/chats-home.component.ts
@@ -57,10 +57,10 @@ export class ChatsHomeComponent {
   onSelectedUser(user:any):void{
     this.selectedUser = user
     if(this.selectedUser){
-      let array = []
-      array = this.container.filter((data:any)=> data.users.some((x:any)=> x == this.selectedUser.username &&this.currentUser.username))
-      this.msgList = array
-      console.log(array)
+      // let array = []
+      // array = this.container.filter((data:any)=> data.users.some((x:any)=> x == this.selectedUser.username &&this.currentUser.username))
+      // this.msgList = array
+      // console.log(array)
     }
     
   }
